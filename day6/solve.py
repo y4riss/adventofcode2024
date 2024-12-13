1f11d4d77034996703a5557feab7a7833d3705fa
joessir <lassiryyass@gmail.com>
2024-12-13 21:25:36 +0100

    solve day6 part2 - very slow bruteforce implementation (50 s to compute answer)

--- day6/solve.py
+++ day6/solve.py
@@ -1,5 +1,6 @@
+from time import time
 grid= []
-with open("sample.txt")as file:
+with open("input.txt")as file:
     for line in file:
         grid.append(list(line.strip()))
 
@@ -52,6 +53,7 @@ def first_part():
     for i in range(n):
         for j in range(m):
             ans += grid[i][j] == 'X'
+   
     print(ans)
             
 
@@ -62,7 +64,85 @@ def first_part():
 
 
 def second_part():
-    pass
+     # idea : try every possible position as obstacle
+
+    LEAVE = 1
+    OBSTACLE = 2
+    POSSIBLE = 3        
+    n = len(grid)
+    m = len(grid[0])
+
+    guard = None
+    initial_guard_pos = None
+    ans = 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == '^':
+                guard = [i,j]
+    initial_guard_pos = guard.copy()
+    
+    directions = [(-1,0),(0,1),(1,0),(0,-1)]
+    def in_bound(i,j):
+        return i >=0 and i < n  and j >=0 and j < m
+
+    def canMove(i, j, currentDirection):
+        dx, dy = directions[currentDirection]
+        next_i = i + dx
+        next_j = j + dy
+        if not in_bound(next_i,next_j):
+            return LEAVE
+        if grid[next_i][next_j] == '#':
+            return OBSTACLE
+        return POSSIBLE
+
+    def move(guard, currentDirection):
+        dx, dy = directions[currentDirection]
+        guard[0] += dx
+        guard[1] += dy
+        return guard
+
+    def isLoop():
+        currentDirection = 0
+        guard = initial_guard_pos.copy()
+        visited = [[-1 for _ in range(m)] for _ in range(n)]
+        exhausted = 0
+        while True:
+
+            i = guard[0]
+            j = guard[1]
+
+            # if guard visited same position with same direction twice
+            if visited[i][j] == currentDirection:
+                return 1
+
+            c = canMove(i, j, currentDirection)
+            if c == POSSIBLE:
+                visited[i][j] = currentDirection
+                exhausted = 0
+                guard = move(guard, currentDirection)
+            elif c == OBSTACLE:
+                currentDirection = (currentDirection + 1) % 4
+                exhausted +=1
+                if exhausted > 3:
+                    return 1
+            else:
+                return 0
+
+    start = time()
+    for i in range(n):
+        for j in range(m):
+            if i == initial_guard_pos[0] and j == initial_guard_pos[1] or grid[i][j] == '#':
+                continue
+            grid[i][j] = '#'
+            ans += isLoop()
+            grid[i][j] = '.'
+
+    end = time()
+    print(f"time took : {end - start} ms")
+    print(ans)
+    
+
+        
 
 
 n = input("part ? > ")
