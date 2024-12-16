1f2c443047a185cf278bdf6f86d5fa1f03312e37
joessir <lassiryyass@gmail.com>
2024-12-16 21:16:43 +0100

    solve day9 - part2

--- day9/solve.py
+++ day9/solve.py
@@ -41,8 +41,74 @@ def first_part():
 
 
 def second_part():
-    pass
 
+    #00...111...2...333.44.5555.6666.777.888899
+    #[2,3]  [12,3] [x,1] [x,1]...
+    #[9,2] , [8,4] , [7,3]...
+
+    # space = [starting_index, length]
+    # elements = [starting_index, length]
+    blocks= []
+    is_file = True
+    current_file = 0
+    spaces = []
+    files = []
+    idx = 0
+    for digit in disk_map:
+
+        if is_file:
+            files.append((idx, digit))
+            idx+=digit
+            for _ in range(digit):
+                blocks.append(current_file)
+        else:
+            spaces.append([idx, digit])
+            idx+=digit
+            for _ in range(digit):
+                blocks.append('.')
+            current_file+=1
+        is_file = not is_file
+
+    n = len(files)
+    i = n - 1
+
+    # check all available spaces from left to right
+    while i >= 0:
+
+        file_idx, file_size = files[i]
+        curr_file = blocks[file_idx]
+
+        for j in range(len(spaces)):
+
+            idx, length = spaces[j]
+
+            # if space is available, fill it
+            if length >= file_size and file_idx > idx:
+                # fill from idx to idx+length with curr_file, and from file_idx to file_idx+file_size with dots
+                for k in range(file_size):
+                    blocks[idx + k] = curr_file
+                    blocks[file_idx + k] = '.'
+                
+                # update space size with rest of space
+                spaces[j][1] -= file_size
+                # update where space starts
+                spaces[j][0] += file_size
+                break
+        i-=1
+
+    ans = 0
+    for idx, x in enumerate(blocks):
+        if x == '.':
+            continue
+        ans += (idx * x)
+    print(ans)
+
+
+   
+
+            
+
+  
 
 option = input("part ? > ")
 if option == '1':
