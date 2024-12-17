a446618ad42f6de5cf8436af882dd1bf5a3a9ff6
joessir <lassiryyass@gmail.com>
2024-12-17 15:51:16 +0100

    solve day10 - part2
    using backtracking

--- day10/solve.py
+++ day10/solve.py
@@ -1,10 +1,19 @@
 g = []
-with open("sample.txt")as file:
+with open("input.txt")as file:
     for line in file:
-        g.append(list(map(int,line.strip())))
+        # g.append(list(map(int,line.strip())))
+        row = []
+        for x in line.strip():
+            if x == '.':
+                row.append(-1)
+            else:
+                row.append(int(x))
+        g.append(row)
+
 
 def first_part():
 
+    print(g)
     n = len(g)
     m = len(g[0])
     ans = 0
@@ -38,7 +47,40 @@ def first_part():
     print(ans)
 
 def second_part():
-    pass
+
+    n = len(g)
+    m = len(g[0])
+    ans = 0
+
+    # dfs, k is current height
+    directions = [(1,0),(-1,0), (0,1),(0,-1)]
+    def explore(i, j, k, visited):
+
+        if not (i >= 0 and i < n and j >= 0 and j < m):
+            return 0
+        
+        if (i,j) in visited:
+            return 0
+        if g[i][j] != k or k > 9:
+            return 0
+
+        if g[i][j] == 9:
+            return 1
+        
+        ans = 0
+        for x,y in directions:
+            visited.add((i,j))
+            ans += explore(i + x , j + y, k + 1, visited)
+            visited.remove((i,j))
+        return ans
+
+    for i in range(n):
+        for j in range(m):
+            if g[i][j] == 0:
+                visited = set()
+                ans+=explore(i,j, 0, visited)
+    print(ans)
+
 
 
 option = input("part ? > ")
